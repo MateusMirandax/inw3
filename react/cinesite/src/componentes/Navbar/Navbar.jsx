67742063279ffa533e124403a184f32a0b4153e3
mateus <duartefiap@gmail.com>
2024-05-13 13:07:29 -0300

    tailwilnd

--- react/cinesite/src/componentes/Navbar/Navbar.jsx
+++ react/cinesite/src/componentes/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
+import {Link} from 'react-router-dom'
 function Navbar() {
     return ( 
-        <h1>Navbar</h1>
+        <nav>
+            <ul>
+               <li><Link to = "/"></Link>Home</li>
+               <li><Link to = "filmes"></Link>Filmes</li>
+               <li><Link to = "sobre"></Link>Sobre</li>
+               <li><Link to = "contato"></Link>Contato</li>
+            </ul>
+        </nav>
      );
 }
 
