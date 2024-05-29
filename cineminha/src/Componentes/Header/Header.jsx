0b951ba00154017216769cf814390bef20c22d81
mateus <duartefiap@gmail.com>
2024-05-29 20:46:54 -0300

    rola

--- cineminha/src/Componentes/Header/Header.jsx
+++ cineminha/src/Componentes/Header/Header.jsx
@@ -2,7 +2,7 @@ import NavBar from "../NavBar/NavBar";
 
 function Header() {
     return ( 
-        <header className="flex justify-around bg-black text-white p-4">
+        <header className="flex justify-around bg-white text-black p-4 border border-purple-600 rounded-lg mt-5">
             <span>Olá, visitante!</span>
             <h1>Bem-vindo ao MoviApp</h1>
             <NavBar/>
--- cineminha/src/Pages/Home.jsx
+++ cineminha/src/Pages/Home.jsx
@@ -1,41 +1,47 @@
-import data from '../../artigos.json'
+import data from "../../artigos.json"
 
 function Home() {
-  console.log(data)
-    return (
-        <>
-        <input className='w-60 h-10 p-5 rounded-lg m-5' type="text" name="busca" id="busca" placeholder='Digite aqui sua busca'/>
-         <div className='grid grid-cols-3'>
+    console.log(data)
+    return ( 
+        <main className="bg-white">
+        <div className="flex justify-center">
+                 <input className=" w-80 h-12 rounded-lg border-solid border-2 border-purple-600 mt-10" type="text" name="busca" id="busca" placeholder="Digite aqui sua busca" />
+            </div>
+            <div className="grid grid-cols-3 gap-4">
 
-           {data.map(filme => (
-              <div className='card' key={filme.title}>
-                <h1>{filme.title}</h1>
-                <img src={filme.image} alt={filme.title}/>
-                <div className=' bg-yellow-300 '>
-                 {filme.tags.map(tag =>(
-                  <span key={tag}className='text-black'> {tag}</span>
-                 ))}
-              </div>
-              <div className='text'>
-                 {filme.text.map(texto =>(
-                  <p key={texto}> {texto}</p>
-                 ))}
-              </div>
-          </div>
+            {data.map(
+                    filme => (
+                     <div className="card mx-5 gap-3" key={filme.title}>
+                           <h1 className="bg-white text-black flex justify-center border-solid border-2 border-purple-600 rounded-lg mt-5 ">{filme.title}</h1>
 
-            ))
-          }
-          </div>
-      </>
-    );
+                           {filme.image ?
+                           <img className="w-40 flex justify-center items-center m-10 rounded-lg mb-15 mt-10 " src={filme.image} alt={filme.title} />:
+                           "Não tem imagem"}
+
+                           <div className="text bg-white rounded-lg text-black border-purple-600 border-2 p-10 mb-7">
+                               {filme.text.map(text => (
+                                <span key={text}> {text}</span>
+                               ))}
+                           </div>
+
+                           <div className="tag bg-white rounded-lg text-black flex justify-center border-pink-700 border-2 mt-4" >
+                               {filme.tags.map(tag => (
+                                <span key={tag}> {tag}</span>
+                               ))}
+                           </div>
+                       
+                           </div>
+                      
+                 
+                )
+                )
+                }
+                
+          
+            </div>
+           
+               
+        </main>
+     );
 }
-
-export default Home ;
-{/* <div className='card'>
-    <h1>Titulo do filme</h1>
-    <img src='/'></img>
-    <div className='tag'>
-        <span>Tags</span>
-    </div>
-    <p>texto do post</p>
-</div> */}
+export default Home;
