1d0d4da1e975c3c587efa696e66e6bcde69f723f
mateus <duartefiap@gmail.com>
2024-05-29 19:38:33 -0300

    rola

--- cineminha/src/Pages/Home.jsx
+++ cineminha/src/Pages/Home.jsx
@@ -1,33 +1,36 @@
 import data from '../../artigos.json'
 
 function Home() {
-console.log(data)
-    return ( 
-        <main>
-            <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca" />
-            <div className='grid grid-cols-3 gap-4'>
-           
-            {data.map(filme => (
-                <div className='card' key={filme.title}>
-                    <h1>{filme.title}</h1>
-                    <img src={filme.image} alt={filme.title} />
-                    <div className='tag'>
-                        {filme.tags.map(tag => (
-                             <span key={tag} className='text-pink-400'>{tag}</span>
-                
-                        ))}
-                       
-                    </div>
-                </div>
-            ))}
-            
-            </div>
-        </main>
-     );
+  console.log(data)
+    return (
+        <>
+        <input className='w-60 h-10 p-5 rounded-lg m-5' type="text" name="busca" id="busca" placeholder='Digite aqui sua busca'/>
+         <div className='grid grid-cols-3'>
+
+           {data.map(filme => (
+              <div className='card' key={filme.title}>
+                <h1>{filme.title}</h1>
+                <img src={filme.image} alt={filme.title}/>
+                <div className=' bg-yellow-300 '>
+                 {filme.tags.map(tag =>(
+                  <span key={tag}className='text-black'> {tag}</span>
+                 ))}
+              </div>
+              <div className='text'>
+                 {filme.text.map(texto =>(
+                  <p key={texto}> {texto}</p>
+                 ))}
+              </div>
+          </div>
+
+            ))
+          }
+          </div>
+      </>
+    );
 }
-export default Home;
 
-
+export default Home ;
 {/* <div className='card'>
     <h1>Titulo do filme</h1>
     <img src='/'></img>
