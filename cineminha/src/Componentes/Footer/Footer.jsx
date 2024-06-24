a6981ee707e170b5f1265106ee36412a6cde6124
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-24 10:04:24 -0300

    commit final

--- cineminha/src/Componentes/Footer/Footer.jsx
+++ cineminha/src/Componentes/Footer/Footer.jsx
@@ -1,17 +1,17 @@
 function Footer() {
     return ( 
         <>
-            <div className="flex justify-between bg-black text-white border-t-2">
+            <div className="pl-5 pr-5 pt-7 pb-7 flex justify-between bg-black text-white border-t-2">
                 <div>
                     <h1>
                         Copyright © 2024 Cine Tropa
                     </h1>
                 </div>
                 <div className="flex">
-                    <h1>
+                    <h1 className="">
                         TERMOS DE PRIVACIDADE
                     </h1>
-                    <h1>
+                    <h1 className="ml-2">
                         TERMOS DE USO
                     </h1>
                 </div>
